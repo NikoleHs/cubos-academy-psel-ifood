3ff416e041045681476689d7d4933ef9342afa1f
Nikole Helechyj Silva <nikole.helechyj@gmail.com>
2021-06-08 21:00:36 -0300

    Celebrações ou descanso?

---
+++ Tem_copa_tem_jogos_nao_MEH.js
@@ -0,0 +1,12 @@
+function solucao(ano) {
+    //seu codigo aqui
+    if (ano % 2 === 0) {
+        if (ano % 4 === 0) {
+            console.log("JOGOS");
+        } else {
+            console.log("COPA");
+        }
+    } else {
+        console.log("MEH");
+    }
+}
